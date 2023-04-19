f9bfddd54c568985dc87bc777c9f54e83e861ab0
hokageCV <chaitanyavaru@gmail.com>
2023-04-20 00:18:05 +0530

    add default host

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ import miscRoutes from "./modules/misc/misc.routes";
 import pragatiRoutes from "./modules/pragati/pragati.routes";
 
 const PORT = 3000;
+const HOST = "0.0.0.0";
 const fastify = Fastify({
   logger: loggerConfig,
 });
@@ -23,7 +24,7 @@ fastify.register(pragatiRoutes, { prefix: "/pragati" });
 // Run the server!
 const start = async () => {
   try {
-    await fastify.listen({ port: PORT });
+    await fastify.listen({ port: PORT, host: HOST });
     console.log(`listening on ${PORT}`);
   } catch (err) {
     fastify.log.error(err);
